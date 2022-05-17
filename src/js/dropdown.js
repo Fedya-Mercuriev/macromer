a0da74a7c25ba418ea1d6169bdca2a77fe694a6b
fedya mercuriev <heyzhongguo@yahoo.com>
2022-05-18 01:18:06 +0700

    Поправлена работа выпадающих списков

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -1,67 +1,48 @@
 (function() {
 	class Dropdowns {
 		constructor(toggleSelector) {
-			this.dropdowns = this._composeDropdownsArray(toggleSelector);
+			this.dropdowns = $(toggleSelector);
 		}
 
-		_composeDropdownsArray(toggleSelector) {
-			const result = [];
-			$(toggleSelector).each(function(_, item) {
-				result.push({
-					toggle: item,
-					dropdown: $(item).next('[js-dropdown-list]'),
-					isOpen: false,
-					open() {
-						$(this.toggle).addClass('is-clicked');
-						$(this.dropdown).addClass('is-open');
-					},
-					close() {
-						$(this.toggle).removeClass('is-clicked');
-						$(this.dropdown).removeClass('is-open');
-					} 
-				});
-			});
-
-			return result;			
+		open($element) {
+			$element.addClass('is-open');
+			$element.find('[js-dropdown-toggle]').addClass('is-clicked');
+			$element.find('[js-dropdown-list]').addClass('is-open');
 		}
 
-		_manageElementsState(targetElement) {
-			let elementIndex = -1;
+		close($element) {
+			$element.removeClass('is-open');
+			$element.find('[js-dropdown-toggle]').removeClass('is-clicked');
+			$element.find('[js-dropdown-list]').removeClass('is-open');
+		}
 
-					this.dropdowns.forEach((item, index) => {
-						if ($(item.toggle).is(targetElement)) {
-							elementIndex = index;
-						}
-					})
-
-					this.dropdowns.forEach((item) => item.close());
-
-					if (elementIndex !== -1) {
-						if (!this.dropdowns[elementIndex].isOpen) {
-							this.dropdowns[elementIndex].open();
-							this.dropdowns[elementIndex].isOpen = true;
-
-						} else {
-							this.dropdowns[elementIndex].close();
-							this.dropdowns[elementIndex].isOpen = false;
-						}
-					}
+		_manageElementsState($element) {
+			const _this = this;
+			if ($element.hasClass('is-open')) {
+				this.dropdowns.each((_, item) => _this.close($(item)));
+				this.close($element);
+			} else {
+				this.dropdowns.each((_, item) => _this.close($(item)));
+				this.open($element);
+			}
 		}
 
 		_bindEvents() {
 			const _this = this;
 
-			this.dropdowns.forEach(function(item) {
-				$(item.toggle).off('.toggle-dropdown').on('click.toggle-dropdown', function(e) {
-					_this._manageElementsState(e.currentTarget);
+			this.dropdowns.each(function(_, item) {
+				debugger;
+				$(item).find('[js-dropdown-list]').off('.toggle-dropdown').on('click.toggle-dropdown', function(e) {
+					e.stopPropagation();
+				});
+				$(item).off('.toggle-dropdown').on('click.toggle-dropdown', function(e) {
+					_this._manageElementsState($(e.currentTarget));
 					e.stopPropagation();
 				})
 			})
 
 			$(window).off('.toggle-dropdown').on('click.toggle-dropdown', function(e) {
-				if (!$(e.currentTarget).closest('[js-dropdown-wrapper]').length) {
-					_this._manageElementsState(e.currentTarget);
-				}
+				_this._manageElementsState($(e.currentTarget));
 			});
 		}
 
@@ -70,11 +51,6 @@
 		}
 	}
 
-	const dropdowns= new Dropdowns('[js-dropdown-toggle]').init();
-
-	// window.off('.toggle-dropdown').on('click.toggle-dropdown', function(e) {
-	// 	if(!$(e.target).closest('[js-dropdown-toggle]')) {
-
-	// 	}
-	// });
+	const dropdowns= new Dropdowns('[js-dropdown-wrapper]');
+	dropdowns.init();
 })();

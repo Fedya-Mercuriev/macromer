097fdd8de5bc28beb916b7a6435a29842c1deda7
fedya mercuriev <heyzhongguo@yahoo.com>
2022-05-30 01:58:46 +0700

    Добавлен плагин для скролла. Добавлен скролл

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -3,11 +3,9 @@
 
     $scrollToTriggers.each(function(_, item) {
         $(item).off('.scroll-to').on('click.scroll-to', function(e) {
-            e.preventDefault();
+            // e.preventDefault();
             const target = $(e.currentTarget).attr('js-scroll-to');
-            $([document.documentElement, document.body]).animate({
-                scrollTop: $(`#${target}`).offset().top
-            }, 800);
+            $(window).scrollTo($(`#${target}`), 800);
         })
     });
 })();
